b70afcd0d1f4056ee89e44b0d4717dbaf2ecf5ca
Maimouna <md6321079@gmail.com>
2021-10-13 13:12:12 +0000

    creation du formulaire

---
+++ formulaire.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>formulaire</title>
+</head>
+<body>
+   <fieldset center>
+    <form method="post"  action="">
+
+    <div>
+        <label for="nom">nom :</label>
+        <input type="text" name="nom" placeholder="Entrez votre nom">
+     </div><br>
+
+     <div>
+        <label for="prenom">prenom :</label>
+        <input type="text" name="prenom" placeholder="Entrez votre prenom">
+     </div><br>
+
+     <div>
+        <label for="mail">mail :</label>
+        <input type="email" name="mail" placeholder="Entrez votre mail">
+     </div><br>
+
+     <div>
+        <label for="password">password:</label>
+        <input type="password" name="password" placeholder="Entrez votre mot de passe">
+     </div><br>
+
+     <div>
+        <input type="submit" name="submit" value="Enregistrer"><br><br>
+     </div>
+     
+    </form>
+    </fieldset> 
+</body>
+</html>
